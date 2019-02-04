5ec2f9547f1eb43f3019fa8c64a7f72b4b9a8ee4
rajivs15 <47183597+rajivs15@users.noreply.github.com>
2019-02-04 08:55:44 +0000

    Add files via upload

---
+++ evenn.py
@@ -0,0 +1,7 @@
+n,k=map(int,input().split())
+for n in range (n+1,k,1):
+    if n%2==0:
+        print(n)
+        
+    
+    
---
+++ palindrome.py
@@ -0,0 +1,12 @@
+n=int(input())
+t=n
+temo=0
+while t!=0:
+    temo=temo*10+t%10
+    t=t//10
+   
+if n==temo:
+    print("yes")
+else:
+    print("NOT")
+    
